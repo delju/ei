3bce89d7e0c488aedf2b9dd8d48fab7b9a35529e
julene <julene.delvaux@gmail.com>
2022-09-16 10:30:26 +0200

    fix bug

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -56,7 +56,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('admin-home'))
     }
 
     protected function getLoginUrl(Request $request): string
